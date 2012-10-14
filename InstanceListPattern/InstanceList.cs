605ac19f9c00174d38227ea7ab0322042a8db9ba
Alexander Brevig <alexanderbrevig@gmail.com>
2012-10-14 22:49:03 +0200

    initial commit

---
+++ InstanceListPattern/InstanceList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InstanceListPattern
+{
+    public class InstanceList<T> : IDisposable where T : class
+    {
+        public static List<T> Instances { get; set; }
+
+        public InstanceList()
+        {
+            if (Instances == null) { Instances = new List<T>(); }
+            Instances.Add(this as T);
+        }
+    
+        public void Dispose()
+        {
+            Instances.Remove(this as T);
+        }
+    }
+}
---
+++ InstanceListPatternSample/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InstanceListPattern;
+
+namespace InstanceListPatternSample
+{
+    class Person : InstanceList<Person>
+    {
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        } 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("p1");
+            Person p2 = new Person("p2");
+
+            PrintAllPersons(); 
+            
+            Person p3 = new Person("p3");
+
+            PrintAllPersons();
+
+            p3.Dispose();
+
+            PrintAllPersons();
+
+            Console.Read();
+        }
+
+        private static void PrintAllPersons()
+        {
+            Console.WriteLine("All instances:");
+            foreach (var p in Person.Instances) {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
